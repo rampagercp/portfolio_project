9c9ef7645a1a43a8296f22b06a133e3f314e45c4
rampagercp <rampagercp@gmail.com>
2021-06-03 20:16:32 -0300

    created partial main content pages

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,21 @@ const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 exports.default = router;
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: 'Home', page: 'home' });
 });
 router.get('/home', function (req, res, next) {
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: 'Home', page: 'home' });
 });
 router.get('/about', function (req, res, next) {
-    res.render('index', { title: 'About' });
+    res.render('index', { title: 'About Me', page: 'about' });
 });
 router.get('/projects', function (req, res, next) {
-    res.render('index', { title: 'Projects' });
+    res.render('index', { title: 'Projects', page: 'projects' });
 });
 router.get('/services', function (req, res, next) {
-    res.render('index', { title: 'Services' });
+    res.render('index', { title: 'Services', page: 'services' });
 });
 router.get('/contact', function (req, res, next) {
-    res.render('index', { title: 'Contact' });
+    res.render('index', { title: 'Contact Me', page: 'contact' });
 });
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -5,40 +5,40 @@ export default router;
 /* GET home page. */
 router.get('/', function(req, res, next) 
 {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home' });
 });
 /* GET home page. */
 router.get('/home', function(req, res, next) 
 {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home'  });
 });
 
 /* GET about page. */
 
 router.get('/about', function(req, res, next) 
 {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: 'About Me', page: 'about'  });
 });
 
 /* GET projects page. */
 
 router.get('/projects', function(req, res, next) 
 {
-  res.render('index', { title: 'Projects' });
+  res.render('index', { title: 'Projects', page: 'projects'  });
 });
 
 /* GET services page. */
 
 router.get('/services', function(req, res, next) 
 {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'Services', page: 'services'  });
 });
 
 /* GET contact page. */
 
 router.get('/contact', function(req, res, next) 
 {
-  res.render('index', { title: 'Contact' });
+  res.render('index', { title: 'Contact Me', page: 'contact'  });
 });
 
 //module.exports = router;
